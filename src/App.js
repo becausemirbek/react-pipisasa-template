22a75806e3344ea7820c965365a3e54ac6976184
mirbekmuratov <mirbekmuratov@MacBook-Air-Mirbek.local>
2020-10-10 01:33:54 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Routes from './routes/Routes';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div id="app">
+      <Routes/>
+    </div>
+  );
+}
+
+export default App;
